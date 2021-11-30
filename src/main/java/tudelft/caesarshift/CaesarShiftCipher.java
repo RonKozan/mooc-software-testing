139efeba3fce046f92f16097ef35f011b61855f5
RonKozan <kozan@computer.org>
2021-11-30 12:46:01 -0500

    fix bug in code re input string shows up in output; also fix to allow handling of spaces; NEED TO CONFIRM no shifting of spaces with CUSTOMER

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -7,20 +7,32 @@ public class CaesarShiftCipher {
         char currentChar;
         int length = message.length();
 
+        // 26 letters in English alphabet; wraparound if needed.
         shift = shift%26;
 
         for(int i = 0; i < length; i++){
             currentChar = message.charAt(i);
            
-            sb.append(currentChar);
-            if (currentChar > 'z' || currentChar < 'a') {
-                return "invalid";
-            } else if ((char) (currentChar + shift) > 'z') {
-                currentChar = (char) (currentChar - 26);
-            } else if ((char) (currentChar + shift) < 'a'){
-                currentChar = (char) (currentChar + 26);
+            //sb.append(currentChar); ERROR FOUND
+
+            // handle spaces (they are not altered)
+            // NOTE: clarify with customer before final submit:
+            // are spaces to be shifted or not?
+            //
+            if (currentChar == ' '){
+                sb.append((char) (currentChar));
             }
-            sb.append((char) (currentChar + shift));
+            else {
+                // valid non-space characters are shifted
+                if (currentChar > 'z' || currentChar < 'a') {
+                    return "invalid";
+                } else if ((char) (currentChar + shift) > 'z') {
+                    currentChar = (char) (currentChar - 26);
+                } else if ((char) (currentChar + shift) < 'a') {
+                    currentChar = (char) (currentChar + 26);
+                }
+                sb.append((char) (currentChar + shift));
+            }
         }
 
         return sb.toString();
