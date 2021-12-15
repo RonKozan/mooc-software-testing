7a19645d0b0557780e5e3d1952c671e04f7e4169
RonKozan <kozan@computer.org>
2021-12-15 11:08:51 -0500

    testcases added and enhanced to find Word Num Count bug

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -7,14 +7,29 @@ public class CountLettersTest {
 
     @Test
     public void multipleMatchingWords() {
-        int words = new CountLetters().count("cats|dogs");
-        Assertions.assertEquals(2, words);
+        int words = new CountLetters().count("tiger|dogs bats");
+        Assertions.assertEquals(3, words);
     }
 
     @Test
     public void lastWordDoesNotMatch() {
         int words = new CountLetters().count("cats|dog");
         Assertions.assertEquals(1, words);
+    }
+    @Test
+    public void validWordAfterSpace() {
+        int words = new CountLetters().count("cats dogr");
+        Assertions.assertEquals(2, words);
+    }
+    @Test
+    public void invalidWordAfterSpace() {
+        int words = new CountLetters().count("Cats dogz");
+        Assertions.assertEquals(1,words);
+    }
+    @Test
+    public void noValidWords() {
+        int words = new CountLetters().count("Catz Dogz sssratz");
+        Assertions.assertEquals(0,words);
     }
 
 }
